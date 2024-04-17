fd6b31d6e03fb7de8ff0e2d514881cb937fd7a8a
Angelleen Sie To <sietoangelleen@gmail.com>
2024-04-17 16:20:49 +0000

    i made wordle

--- sketch.js
+++ sketch.js
@@ -1,9 +1,28 @@
 function setup() {
-    createCanvas(400, 400);
-  }
+  createCanvas(400, 400);
+}
+
+function hint(guess){
+  var colors = ["🖤","🖤","🖤","🖤","🖤"]
+  for(var i =0; i < guess.length; i++){
+    if (guess[i]==secret_word[i]){
+      colors[i]="💚";
+    }
+}
+  return colors;
+}
   
-  function draw() {
-    background(220);
-    background("lightblue");
-rect (100,100,200,200)
-  }
+var secret_word ="foyer";
+  var user_guess= "";
+for (var i=0;i<6;i++){
+var generated_hint = hint(user_guess)
+
+  user_guess= prompt(generated_hint + "what word do you guess?");
+
+if (user_guess == secret_word)
+  alert("Congrats you won!")
+}
+function draw() {
+
+  background(220);
+}
